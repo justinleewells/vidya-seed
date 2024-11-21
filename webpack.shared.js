19b232258261fc7ba59cd505f524843cddfe6e5d
Justin L. Wells <justinleewells@icloud.com>
2024-11-20 18:02:51 -0600

    Automatically register type aliases

--- webpack.shared.js
+++ webpack.shared.js
@@ -1,5 +1,17 @@
+const fs = require('fs')
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const alias = {
+  '@enums': path.resolve(__dirname, 'src/enums/'),
+  '@game-objects': path.resolve(__dirname, 'src/game-objects'),
+  '@shared': path.resolve(__dirname, 'src/shared'),
+  '@src': path.resolve(__dirname, 'src'),
+  '@utils': path.resolve(__dirname, 'utils'),
+}
+fs.readdirSync('./src/types').forEach((type) => {
+  alias[`@${type}`] = path.resolve(__dirname, `src/types/${type}`)
+})
 
 module.exports = {
   entry: './src/index.js',
@@ -19,15 +31,7 @@ module.exports = {
       },
     ],
   },
-  resolve: {
-    alias: {
-      '@enums': path.resolve(__dirname, 'src/enums/'),
-      '@game-objects': path.resolve(__dirname, 'src/game-objects'),
-      '@shared': path.resolve(__dirname, 'src/shared'),
-      '@src': path.resolve(__dirname, 'src'),
-      '@utils': path.resolve(__dirname, 'utils'),
-    },
-  },
+  resolve: { alias },
   optimization: {
     splitChunks: {
       chunks: 'all',
